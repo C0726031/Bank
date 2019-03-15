f9b3e0566a3c98ccc7cfa14b5ed3507ef6a38656
visualstudio <visualstudio@122-PC22>
2019-03-15 17:44:47 -0400

    commit3

---
+++ BankTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+        [TestClass]
+        public class BankAccountTests
+        {
+            [TestMethod]
+            public void TestMethod()
+            { }
+
+        }
+    }
+}
